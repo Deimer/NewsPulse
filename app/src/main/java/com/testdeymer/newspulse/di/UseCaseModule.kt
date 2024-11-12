d284c26d4a1de1ef60c6f0943ca77a4ee9a865a7
Deimer <deimer215@gmail.com>
2024-11-12 00:18:18 -0500

    The UseCaseModule object class is implemented to provide use cases with dependency injection.

---
+++ app/src/main/java/com/testdeymer/newspulse/di/UseCaseModule.kt
@@ -0,0 +1,34 @@
+package com.testdeymer.newspulse.di
+
+import com.testdeymer.repository.repositories.IHitRepository
+import com.testdeymer.usecase.hit.DeleteHitByIdUseCase
+import com.testdeymer.usecase.hit.FetchAllHitsUseCase
+import com.testdeymer.usecase.hit.FetchHitByIdUseCase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.scopes.ViewModelScoped
+
+@Module
+@InstallIn(ViewModelComponent::class)
+object UseCaseModule {
+
+    @Provides
+    @ViewModelScoped
+    fun provideFetchHitByIdUseCase(
+        hitRepository: IHitRepository
+    ) = FetchHitByIdUseCase(hitRepository)
+
+    @Provides
+    @ViewModelScoped
+    fun provideFetchAllHitsUseCase(
+        hitRepository: IHitRepository
+    ) = FetchAllHitsUseCase(hitRepository)
+
+    @Provides
+    @ViewModelScoped
+    fun provideDeleteHitByIdUseCase(
+        hitRepository: IHitRepository
+    ) = DeleteHitByIdUseCase(hitRepository)
+}
