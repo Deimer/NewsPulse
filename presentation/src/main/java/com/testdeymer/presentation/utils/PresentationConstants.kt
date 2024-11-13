99945bbd51a9780b7edf9d276c408ebfb4878da4
Deimer <deimer215@gmail.com>
2024-11-12 20:10:38 -0500

    Navigation is implemented with NavHost.

---
+++ presentation/src/main/java/com/testdeymer/presentation/utils/PresentationConstants.kt
@@ -0,0 +1,13 @@
+package com.testdeymer.presentation.utils
+
+import com.airbnb.lottie.compose.LottieConstants
+
+object PresentationConstants {
+
+    object AnimationConstants {
+        const val ITERATION_FOREVER = LottieConstants.IterateForever
+        const val ITERATION_SIMPLE = 1
+        const val TRANSITION_DURATION = 700
+        const val TRANSITION_DURATION_SCROLL = 6000L
+    }
+}
